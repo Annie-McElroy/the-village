1de05da980f02b70ca864d0d3226ae9aeacd6bab
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-09-08 13:13:54 -0500

    Added comments to new code

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
+// Import GraphQL schemas and db connection
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+// Set up Port and Apollo Server
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -18,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')))
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
@@ -28,7 +30,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
+// Create new instance of Apollo Server with the GraphQL schema
 const startApolloServer = async () => {
     await server.start();
     server.applyMiddleware({ app });
