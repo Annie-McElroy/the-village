1bcf4984a3e94aaef44227a2bc0aefc70c40ff71
Dahlia Guido <dguido.dev@gmail.com>
2023-09-09 17:38:29 -0500

    added the larger versions of banner images, added the single request component and the component, Nav bar will appear only if use is logged in

--- client/src/pages/Village.js
+++ client/src/pages/Village.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import DisplayRequest from './Request';
 import ClaimRequestButton from '../components/ClaimRequestButton';
+import CreateRequest from '../components/CreateRequest';
 
 const Village = () => {
   return (
     <div className="container">
       <DisplayRequest />
       <ClaimRequestButton />
+      <CreateRequest />
       
     </div>
 
