4e4500f2638c071dc01d631dc410b7426505627c
Valeriereds <valeriereds@gmail.com>
2023-09-08 22:05:32 -0500

    Added Village page layout

---
+++ client/src/components/DisplayRequest/index.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import FormControl, { useFormControl } from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormHelperText from '@mui/material/FormHelperText';
+
+function MyFormHelperText() {
+  const { focused } = useFormControl() || {};
+
+  const helperText = React.useMemo(() => {
+    if (focused) {
+      return 'Comment here!';
+    }
+
+    return 'commenting...';
+  }, [focused]);
+
+  return <FormHelperText>{helperText}</FormHelperText>;
+}
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+function ChildModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button onClick={handleOpen}>Comment</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style, width: 200 }}>
+        <form noValidate autoComplete="off">
+      <FormControl sx={{ width: '25ch' }}>
+        <OutlinedInput placeholder="Please enter text" />
+        <MyFormHelperText />
+      </FormControl>
+    </form>
+          <Button onClick={handleClose}>Submit Comment</Button>
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export default function NestedModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Display Request</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <h2 id="parent-modal-title">Carpool?</h2>
+          <p id="parent-modal-description">
+            Can someone drop my kids off at school tomorrow?
+          </p>
+          <ChildModal />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,19 @@
-import React from "react";
-// import LoginForm from "../components/LoginForm";
-import Crayon from "../components/Crayons";
-import ClaimRequestButton from "../components/ClaimRequestButton";
+// import React from "react";
+// // import LoginForm from "../components/LoginForm";
+// import Crayon from "../components/Crayons";
+// import ClaimRequestButton from "../components/ClaimRequestButton";
 
 
-const Home = () => {
-  return (
-    <div className="container">
-      {/* <Nav /> */}
-      <Crayon />
-      <ClaimRequestButton />
-    </div>
-  );
-};
 
-export default Home;
+// const Home = () => {
+//   return (
+//     <div className="container">
+//       <Crayon />
+//       <ClaimRequestButton />
+      
+//     </div>
+
+//   );
+// };
+
+// export default Home;
