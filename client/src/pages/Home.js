af5bae6e227368a4e69cf93bb42815c5e11416ea
Dahlia Guido <dguido.dev@gmail.com>
2023-09-08 22:15:27 -0500

    testing the modal comments and draft navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,21 @@
-// import React from "react";
-// // import LoginForm from "../components/LoginForm";
-// import Crayon from "../components/Crayons";
-// import ClaimRequestButton from "../components/ClaimRequestButton";
+import React from "react";
+// import LoginForm from "../components/LoginForm";
+import Crayon from "../components/Crayons";
+import ClaimRequestButton from "../components/ClaimRequestButton";
 
 
+const Home = () => {
+  return (
+    <div className="landing-page">
+      <div className="div" style={{ position: 'relative' }}>
+        <img className="layered-village base" alt="Layered village" src="/icons/layered-village-base-1.png" />
+        <img className="layered-village foreground" alt="Layered village" src="/icons/layered-village-foreground-1.png" />
+        <img className="layered-village clouds" alt="Layered village" src="/icons/layered-village-clouds-1.png" />
+        <Crayon />
+        <ClaimRequestButton />
+      </div>
+    </div>
+  );
+};
 
-// const Home = () => {
-//   return (
-//     <div className="container">
-//       <Crayon />
-//       <ClaimRequestButton />
-      
-//     </div>
-
-//   );
-// };
-
-// export default Home;
+export default Home;
