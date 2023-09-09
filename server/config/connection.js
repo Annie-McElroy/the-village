7cf2ee99c85538af1fe354e27bd4ac42bafe7273
Abe Omaits <aomaits@gmail.com>
2023-09-09 16:06:24 -0500

    "merging changes"

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Setup connection with URI hidden in .env file
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.kauqu1v.mongodb.net/the-village');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.s6pohbg.mongodb.net/the-village');
 
 module.exports = mongoose.connection;
