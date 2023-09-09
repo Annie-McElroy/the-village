ff4029279961cc5127293569f326439f5beb14bf
Dahlia Guido <dguido.dev@gmail.com>
2023-09-08 22:51:54 -0500

    banner images are layered and nav bar is fixed at the bottom of the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Village from './pages/Village';
+import Home from './pages/Home';
 import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <div><Home /></div>
       <div><Village /></div>
       <footer><Nav /></footer>
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ import ClaimRequestButton from "../components/ClaimRequestButton";
 const Home = () => {
   return (
     <div className="landing-page">
-      <div className="div" style={{ position: 'relative' }}>
+      <div className="hero-banner" style={{ position: 'relative' }}>
         <img className="layered-village base" alt="Layered village" src="/icons/layered-village-base-1.png" />
         <img className="layered-village foreground" alt="Layered village" src="/icons/layered-village-foreground-1.png" />
         <img className="layered-village clouds" alt="Layered village" src="/icons/layered-village-clouds-1.png" />
