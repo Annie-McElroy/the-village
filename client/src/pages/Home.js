70871d31e2b34a5753f705259af0df39da400b9f
Dahlia Guido <dguido.dev@gmail.com>
2023-09-11 14:20:16 -0500

    working layout positioning for homepage and addying typography style rules for village theme

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,18 +9,25 @@ import './styles/home.css'
 const Home = () => {
   return (
     <div className="landing-page">
-      <div className="banner">
-        <div className="layered-village base" alt="base"></div>
-        <div className="layered-village foreground" alt="foreground"></div>
-        <div className="layered-village clouds" alt="foreground"></div>
+      <div className="div">
 
-      </div>
-      <LoginForm />
-      {/* {AuthService.loggedIn()
+        <div className="banner">
+          <div className="layered-village base" alt="base"></div>
+          <div className="layered-village foreground" alt="foreground"></div>
+          <div className="layered-village clouds" alt="foreground"></div>
+        </div>
+
+        <div className="text-wrapper">Welcome to the Village!</div>
+        <p className="p">Connect to your village today!</p>
+        <p className="text-wrapper-2">Already a Villager? Welcome back!</p>
+        <LoginForm  className="login-CTA-2"/>
+        {/* {AuthService.loggedIn()
         ? (<div>Logged In!</div>)
         : (<LoginForm />)
       } */}
-      <div style={{ height: '80px' }}></div>
+        <div style={{ height: '80px' }}></div>
+
+      </div>
     </div>
   );
 };
