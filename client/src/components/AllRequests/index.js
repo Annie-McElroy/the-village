6e6c68aa9c440247eb1c7d9a7023f10a26ec9c9c
Dahlia Guido <dguido.dev@gmail.com>
2023-09-12 10:06:30 -0500

    Group effort getting the all request to show in village page

--- client/src/components/AllRequests/index.js
+++ client/src/components/AllRequests/index.js
@@ -34,9 +34,9 @@ export default function VillageRequests() {
   
   const { loading, data } = useQuery(QUERY_ALL_REQUEST);
   const requests = data?.requests; 
- console.log(requests);
- console.log(data);
-
+ console.log('this is the requests ' + requests);
+ console.log('this is the data ' + data);
+ console.log('the is QUERY_ALL_REQUEST ' + QUERY_ALL_REQUEST)
   return (
     <div>
       {loading ? (
@@ -45,7 +45,7 @@ export default function VillageRequests() {
         requests ? (
         requests.map((request) =>(
           <Card key={request.id}>
-            <Typography>{request.authorId}</Typography>
+            <Typography>{request.authorId.username}</Typography>
             <Typography>{request.title}</Typography>
             <Typography>{request.body}</Typography>
             <Typography>{request.crayons}</Typography>
