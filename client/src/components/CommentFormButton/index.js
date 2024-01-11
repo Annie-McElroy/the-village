87197b582efe7def447b651e6c460ced9b82d319
Abe Omaits <aomaits@gmail.com>
2024-01-11 12:18:05 -0600

    Added a SUBMIT COMMENT button to the single request page that we will later hook up to be able to add comments from the client-side.

---
+++ client/src/components/CommentFormButton/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+export default function CommentFormButton() {
+    return (
+
+        <Button variant="contained">
+            Submit Comment
+        </Button>
+    );
+}
