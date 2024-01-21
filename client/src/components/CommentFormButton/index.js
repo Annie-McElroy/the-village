cf2fbe21038856485202e5a7a4bb630b1447af05
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2024-01-21 12:05:07 -0600

    Comment mutation runs and works, but is not rendering comment right away

--- client/src/components/CommentFormButton/index.js
+++ client/src/components/CommentFormButton/index.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
-export default function CommentFormButton() {
+export default function CommentFormButton({ onClick }) {
     return (
 
-        <Button variant="contained">
+        <Button variant="contained" onClick={onClick}>
             Submit Comment
         </Button>
     );
