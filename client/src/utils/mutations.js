9023a7408cdfb4fe7f770f313183c070487446e7
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2024-02-28 11:00:03 -0600

    Added UpdateRequest mutation to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -160,3 +160,21 @@ mutation DeleteRequest($id: ID!) {
   }
 }
 `
+
+export const UPDATE_REQUEST = gql`
+mutation UpdateRequest($id: ID!, $title: String, $body: String, $crayon: Int) {
+  updateRequest(_id: $id, title: $title, body: $body, crayon: $crayon) {
+    _id
+    authorId {
+      _id
+      username
+    }
+    body
+    crayons
+    createdAt
+    isClaimed
+    isComplete
+    title
+  }
+}
+`
